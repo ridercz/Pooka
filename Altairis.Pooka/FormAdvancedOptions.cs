ee016d9df142ba61a7668e25cee15e65c51be926
Michal Altair Valášek <github@rider.cz>
2024-02-17 20:14:32 +0100

    Upgrade to .NET 8.0

--- Altairis.Pooka/FormAdvancedOptions.cs
+++ Altairis.Pooka/FormAdvancedOptions.cs
@@ -10,7 +10,7 @@ public partial class FormAdvancedOptions : Form {
     }
 
     private void buttonSave_Click(object sender, EventArgs e) {
-        Settings.Set(Settings.ShutdownMethod, this.comboMethod.SelectedItem.ToString() ?? Settings.ShutdownMethodDefault);
+        Settings.Set(Settings.ShutdownMethod, this.comboMethod.SelectedItem?.ToString() ?? Settings.ShutdownMethodDefault);
         Settings.Set(Settings.WatchFileInterval, (int)this.numericInterval.Value);
         this.Close();
     }
